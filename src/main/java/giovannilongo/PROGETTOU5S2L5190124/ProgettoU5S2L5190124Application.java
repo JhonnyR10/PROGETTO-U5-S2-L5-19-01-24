2d51d5aeb404c6126d87e765ea7555c1c8be9850
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-19 11:28:17 +0100

    Prima impostazione

---
+++ src/main/java/giovannilongo/PROGETTOU5S2L5190124/ProgettoU5S2L5190124Application.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTOU5S2L5190124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5S2L5190124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5S2L5190124Application.class, args);
+	}
+
+}
---
+++ src/test/java/giovannilongo/PROGETTOU5S2L5190124/ProgettoU5S2L5190124ApplicationTests.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTOU5S2L5190124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5S2L5190124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
