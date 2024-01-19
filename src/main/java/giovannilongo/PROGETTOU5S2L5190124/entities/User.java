23b96e9fc6eb18994ad3c15fb75b3e18241a61c0
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-19 11:46:53 +0100

    entities

---
+++ src/main/java/giovannilongo/PROGETTOU5S2L5190124/entities/User.java
@@ -0,0 +1,38 @@
+package giovannilongo.PROGETTOU5S2L5190124.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String avatar;
+    @OneToMany(mappedBy = "user")
+    private List<Device> devices;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
